fd1f102e6f6e5bea6e35b0ed55d6dce7dbe2a67c
Gulchaman Mammadova <gulchaman.mammadova@gmail.com>
2021-06-02 22:38:54 -0400

    Finished Phone Version of Lab3

--- app/src/main/java/algonquin/cst2335/mamm0012/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mamm0012/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -16,61 +19,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView mytext = findViewById(R.id.textview);
-        Button btn = findViewById(R.id.mybutton);
-        CheckBox checkBox = findViewById(R.id.mycheckbox);
-        Switch switchbtn = findViewById(R.id.myswitch);
-        RadioButton radioButton = findViewById(R.id.myradiobutton);
-        ImageView myimage = findViewById(R.id.logoalgonquin);
-        ImageButton imgbtn = findViewById(R.id.myimagebutton);
-        EditText myedit = findViewById(R.id.myedittext);
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
 
-        if (btn !=null) {
-            btn.setOnClickListener(v -> {
-                String editString = myedit.getText().toString();
-                mytext.setText("Your edit text has:" + editString);
-            });
-        }
+        sw.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            String toastMessage = "You clicked on the Checkbox and it is now: " + isChecked;
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, toastMessage, duration);
-            toast.show();
+                imgView.startAnimation(rotate);
+            } else {
+                imgView.clearAnimation();
+            }
+
         });
 
-        switchbtn.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            String toastMessage = "You clicked on the Switch button and it is now: " + isChecked;
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, toastMessage, duration);
-            toast.show();
-        });
-
-        radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            String toastMessage = "You clicked on the Radio button and it is now: " + isChecked;
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, toastMessage, duration);
-            toast.show();
-        });
-
-        imgbtn.setOnClickListener(v -> {
-            int width = v.getWidth();
-            int height = v.getHeight();
-            String toastMessage = "The width = " + width + " and height = " + height;
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, toastMessage, duration);
-            toast.show();
-        });
 
 
     }
