b01e20b4108342f6739edfa42dd62866d5a1f1f9
Gulchaman Mammadova <gulchaman.mammadova@gmail.com>
2021-06-17 23:18:11 -0400

    finished week Creating a ViewHolder

--- app/src/main/java/algonquin/cst2335/mamm0012/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/mamm0012/ChatRoom.java
@@ -1,46 +1,125 @@
 package algonquin.cst2335.mamm0012;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 public class ChatRoom extends AppCompatActivity {
     RecyclerView chatList;
+    ArrayList<ChatMessage> messages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.chatlayout );
+
         chatList = findViewById(R.id.myrecycler);
-        chatList.setAdapter(new MyChatAdapter());
+
+        MyChatAdapter adt = new MyChatAdapter();
+        chatList.setAdapter(adt);
+        chatList.setLayoutManager(new LinearLayoutManager(this));
+
+        Button sendButton = findViewById(R.id.sendButton);
+        Button receiveButton = findViewById(R.id.receiveButton);
+        TextView messageTextView = findViewById(R.id.messageTextView);
+        sendButton.setOnClickListener(clk -> {
+            ChatMessage message = new ChatMessage(messageTextView.getText().toString(),1,new Date());
+            messages.add(message);
+            messageTextView.setText("");
+
+            adt.notifyItemInserted(messages.size() - 1);
+        });
+        receiveButton.setOnClickListener(clk -> {
+            ChatMessage message = new ChatMessage(messageTextView.getText().toString(),2,new Date());
+            messages.add(message);
+            messageTextView.setText("");
+
+            adt.notifyItemInserted(messages.size() - 1);
+        });
     }
 
     private class ChatMessage {
         String message;
         int sendOrReceive;
         Date timeSent;
+
+        public ChatMessage(String message, int sendOrReceive, Date timeSent) {
+            this.message = message;
+            this.sendOrReceive = sendOrReceive;
+            this.timeSent = timeSent;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public int getSendOrReceive() {
+            return sendOrReceive;
+        }
+
+        public String getTimeSent() {
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a", Locale.getDefault());
+            return sdf.format(timeSent);
+        }
     }
 
-    private class MyChatAdapter extends RecyclerView.Adapter {
+    private class MyRowViews extends RecyclerView.ViewHolder {
+        TextView messageText;
+        TextView timeText;
+
+        public MyRowViews(View itemView) {
+            super(itemView);
+
+            messageText = itemView.findViewById(R.id.message);
+            timeText = itemView.findViewById(R.id.time);
+        }
+    }
+
+    private class MyChatAdapter extends RecyclerView.Adapter<MyRowViews> {
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+        public MyRowViews onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = getLayoutInflater();
+            int layoutID;
+            if (viewType == 1) // Send
+                layoutID = R.layout.sent_message;
+            else // Receive
+                layoutID = R.layout.receive_message;
+            View loadedRow = inflater.inflate(layoutID, parent, false);
+            MyRowViews initRow = new MyRowViews(loadedRow);
+            return initRow;
         }
 
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        public void onBindViewHolder(MyRowViews holder, int position) {
+            holder.messageText.setText(messages.get(position).getMessage());
+            holder.timeText.setText(messages.get(position).getTimeSent());
+        }
 
+        @Override
+        public int getItemViewType(int position) {
+            ChatMessage message = messages.get(position);
+            return message.sendOrReceive;
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return messages.size();
         }
     }
+
+
 }
